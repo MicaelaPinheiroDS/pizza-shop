dc02f9aa1abd74218f53bd8778246d874cac2f4f
Micaela Pinheiro <micaela.mews@gmail.com>
2024-03-12 12:51:36 -0300

    fix filename import account menu component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Separator } from '@radix-ui/react-separator'
 import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
 
-import { AcconuntMenu } from './account.menu'
+import { AcconuntMenu } from './accountMenu'
 import { NavLink } from './navLink'
 import { ModeToggle } from './theme/theme-toggle'
 
